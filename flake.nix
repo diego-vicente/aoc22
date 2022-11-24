dfbc6766a2532ac9b368a7d3fdf3669a700f03f6
Diego Vicente <mail@diego.codes>
2022-11-24 12:33:40 +0100

    Add boilerplate code

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A flake for the Advent of Code 2022 development";
+
+  inputs ={
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let 
+          pkgs = import nixpkgs { 
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [ 
+              go
+              gotools
+              # Tools for development
+              golangci-lint
+              gopls
+              delve
+              # go-outline
+              # gopkgs
+            ];
+
+            # shellHook = ''
+            # '';
+          };
+        }
+      );
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diego-vicente/aoc22
+
+go 1.19
