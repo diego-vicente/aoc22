641af7c7d1bce3015fdd21a4c76b5386f2dde3c9
Diego Vicente <mail@diego.codes>
2022-11-25 21:53:20 +0100

    Include path argument in main

--- main.go
+++ main.go
@@ -7,9 +7,17 @@ import (
 
 func main() {
 	day := flag.Int("day", 1, "day to solve")
+	path := flag.String("input", "", "path to input file")
 
 	flag.Parse()
 
 	fmt.Println("Welcome to Advent of Code 2022!")
 	fmt.Println("You have asked to solve day:", *day)
+
+	switch *day {
+	// case 1:
+	// 	aoc.SolveDay01(*path)
+	default:
+		fmt.Printf("Day %d has not been implemented yet.", day)
+	}
 }
